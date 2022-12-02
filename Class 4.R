7eb159cd7c9199f064ca1b0346534a0e1a56727c
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2022-12-02 01:06:54 -0500

    Class 4 - Case study: model and prediction evaluation

--- Class 4.R
+++ Class 4.R
@@ -82,7 +82,7 @@ d_new |> arrange(lp)
 # other values is a rank defining who may take the longest or the shortest
 # time to the event
 
-# Calculating a full predicition for new data (not relative to others)
+# Calculating a full prediction for new data (not relative to others)
 d_luigi <- filter(d_new, name == "Carine")
 M1.surv <- survfit(M1, newdata = d_luigi)
 M1.surv
@@ -106,3 +106,70 @@ d_testing <- read_csv("~/Downloads/DSTI_survey.csv") |>
       `Year of birth`, age)) |>
   transmute(age, gender = sex, smkEver, smkAge)
 d_testing
+
+library(asaur)
+d_training <- pharmacoSmoking
+
+# Model training
+M1 <- coxph(Surv(ttr, relapse) ~ age + gender, data = d_training)
+summary(M1)
+M2 <- coxph(Surv(ttr, relapse) ~ age, data = d_training)
+summary(M2)
+M3 <- coxph(Surv(ttr, relapse) ~ gender, data = d_training)
+summary(M3)
+
+# Making predictions for the DSTI dataset
+d_testing$pred_m1 <- predict(M1, newdata = d_testing)
+d_testing$pred_m2 <- predict(M2, newdata = d_testing)
+d_testing$pred_m3 <- predict(M3, newdata = d_testing)
+
+d_testing
+
+# Validating the quality of the predictions
+summary(coxph(Surv(smkAge, smkEver) ~ pred_m1, data = d_testing))
+# p-value is low so it is not significant: there is no association between
+#   prediction and risk
+# the coefficient is negative. The expectation should be the higher the score
+#   then the higher the risk. However in this case means, the higher the score
+#   then the lower the risk so it is in the wrong direction
+# Concordance (c statistic - part of the output) is the fraction of pairs that
+#   are correctly sorted so it is analogoius to the AUC so it is another
+#   measure of performance between models, the higher the better
+
+summary(coxph(Surv(smkAge, smkEver) ~ pred_m2, data = d_testing))
+summary(coxph(Surv(smkAge, smkEver) ~ pred_m3, data = d_testing))
+# None of the models provides a valid prediction
+
+# As an alternative, we can discretize the predictions
+hist(d_testing$pred_m1)
+d_testing <- d_testing |>
+  mutate(m1_risk = factor(pred_m1 > median(pred_m1, na.rm = TRUE),
+                          levels = c(FALSE, TRUE),
+                          labels = c("low", "high")))
+
+table(d_testing$m1_risk)
+filter(d_testing, is.na(pred_m1))
+
+survfit(Surv(smkAge, smkEver) ~ m1_risk, data = d_testing) |>
+  plot(col = 1:2, fun = "F", xlab = "Age (years)", ylab = "Cumulated
+  probability of smoking")
+  legend("bottomright", col = 1:2, legend = c("low risk", "high risk"), lty = 1)
+
+survdiff(Surv(smkAge, smkEver) ~ m1_risk, data = d_testing)
+# LogRank test: p-value  = 0.5 so there is no significant difference between
+# low and high risk predictions
+summary(coxph(Surv(smkAge, smkEver) ~ m1_risk, data = d_testing))
+# the risk of high risk is 33% higher than low risk group
+
+# Another option: binarize the duration
+d_bin <- d_testing |> mutate(
+  y = ifelse(
+    smkAge > 20, 0,
+    ifelse (
+      smkAge <= 20 & smkEver, 1, NA)
+  )
+)
+d_bin
+# Now this is a classification problem. However, the challenge is that
+# now we are limiting ourselves to a specific duration
+
